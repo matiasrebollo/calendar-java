1f564e4d5415d060299c31c2502e29bafe5be2eb
AgustinPerezR <aperezr@fi.uba.ar>
2023-04-13 15:14:39 -0300

    nuevos metodos de Tarea

--- src/main/java/Calendario.java
+++ src/main/java/Calendario.java
@@ -1,10 +1,13 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Calendario {
     private ArrayList<Evento> eventos;
     private ArrayList<Tarea> tareas;
     public Calendario(){
-
+        this.eventos = new ArrayList<>();
+        this.tareas = new ArrayList<>();
     }
     public void crearEvento() {
         var evento = new Evento();
@@ -16,8 +19,8 @@ public class Calendario {
     public void eliminarEvento(Evento evento) {
         eventos.remove(evento);
     }
-    public void crearTarea(String titulo, String descripcion, boolean todoElDia) {
-        var tarea = new Tarea(titulo, descripcion,todoElDia);
+    public void crearTarea(String titulo, String descripcion, Date fecha, boolean todoElDia) {
+        var tarea = new Tarea(titulo, descripcion, fecha, todoElDia);
         tareas.add(tarea);
     }
     public void modificarTarea() {
--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -1,25 +1,72 @@
-import java.time.LocalDate;
 import java.util.Date;
 
 public class Tarea {
+    private static final int ERROR = -1;
+    private static final int OK = 0;
     private String titulo;
     private String descripcion;
     private boolean todoElDia;
-    private LocalDate vencimiento;
+    private Date fecha;
+    //LocalTime hora;
+    private int[] horario = new int[2];// [0] = hs,    [1] = min
+
     private boolean completada;
 
-    public Tarea(String titulo, String descripcion, boolean todoElDia) {
+    public Tarea(String titulo, String descripcion,Date fecha, boolean todoElDia) {
+        this.completada = false;
         this.titulo = titulo;
         this.descripcion = descripcion;
-        this.completada = false;
+        this.fecha = fecha;
         if (todoElDia) {
             this.todoElDia = true;
         }
         else {
             this.todoElDia = false;
-            //this.vencimiento = vencimiento;
-        }
+            //this.horario = horario;
+         }
     }
 
+    /**
+     * Recibe un horario nuevo y modifica el actual
+     * si pudo modificarlo devuele 0
+     * si hubo un error devuelve -1
+     */
+    public int modificarHorario(int[] nuevoHorario) {
+        if (nuevoHorario[0] >= 24 || nuevoHorario[1] >= 60) {
+            return ERROR;
+        }
+        else if (nuevoHorario[0] < 0 || nuevoHorario[1] < 0) {
+            return ERROR;
+        }
+        this.horario[0] = nuevoHorario[0];
+        this.horario[1] = nuevoHorario[1];
+        return OK;
+    }
+    public void modificarFecha(Date fechaNueva) {
+        this.fecha = fechaNueva;
+    }
+    public void modificarTitulo(String tituloNuevo) {
+        this.titulo = tituloNuevo;
+    }
+    public void modificarDescripcion(String descripcionNueva) {
+        this.descripcion = descripcionNueva;
+    }
+    /**
+     * Si la tarea está completada, la marca como no completada
+     * si la tarea no está completada, la marca como completada
+     * Devuelve true si al final queda como completada o false en caso contrario
+     */
+    public boolean marcarTareaCompletada() {
+        if (this.completada == false) {
+            this.completada = true;
+        }
+        else {
+            this.completada = false;
+        }
+        return this.completada;
 
+    }
 }
+
+
+
